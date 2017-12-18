046e88de018b2e923ee1828b5ad4a8a4218f459c
Aaron <aaronrp1998@gmail.com>
2017-12-18 12:47:31 +0100

    c1

--- src/js/Boot.js
+++ src/js/Boot.js
@@ -1,21 +1,27 @@
-var Boot = Boot || {};
+var Game = {};
 
-Boot = {
-  preload: function() {
-    clicBoton: '',
-    YoungMan.load.image('pantallaInicialFondo', 'images/pantallaInicialFondo.jpg');
-    YoungMan.load.image('pantallaInicialBotones', 'sprites/pantallaInicialBotones.png');
-    YoungMan.load.audio('pantallaInicialMusica', 'audio/pantallaInicialMusica.mp3');
-    YoungMan.load.audio('pantallaInicialClic', 'audio/pantallaInicialClic.mp3');
+Game.Boot = function(game){
+
+};
+
+Game.Boot.prototype = {
+  init:function() {
+
+    this.input.maxPointers = 1;
+    this.stage.disableVisibilityChange = true;
+
   },
-  create: function() {
-    YoungMan.add.sprite(0, 0, 'pantallaInicialFondo');
-    var musica = YoungMan.add.audio('pantallaInicialMusica');
-    musica.play('', 0, 0.5, true);
-    this.clicBoton = YoungMan.add.audio('pantallaInicialClic');
-    YoungMan.add.button(960, 540, 'pantallaInicialBotones', this.iniciar, this);
+
+  preload:function() {
+
+    this.load.image('preloaderBar', 'images/preloader.png');
+
   },
-  iniciar: function(){
-    console.log('Inicia videojuego');
+
+  create:function() {
+
+    this.state.start('Preloader');
   }
+
+
 }

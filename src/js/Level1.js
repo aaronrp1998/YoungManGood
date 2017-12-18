e9788b092301403304c5a54680ded7a2c623d6f1
Aaron <aaronrp1998@gmail.com>
2017-12-18 20:01:04 +0100

    c126

--- src/js/Level1.js
+++ src/js/Level1.js
@@ -21,7 +21,7 @@ Game.Level1.prototype = {
     layer.resizeWorld();
     map.setCollisionBetween(0,39);
 
-    player = this.add.sprite(100, 800, 'player');
+    player = this.add.sprite(100, 1800, 'player');
     player.anchor.setTo(0.5,0.5);
 
     this.physics.arcade.enable(player);

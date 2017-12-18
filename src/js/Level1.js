5328df4df49b05e72fa18bf575b0ed3d7591d114
Aaron <aaronrp1998@gmail.com>
2017-12-18 23:58:54 +0100

    c155

--- src/js/Level1.js
+++ src/js/Level1.js
@@ -12,7 +12,8 @@ Game.Level1.prototype = {
 
   create:function() {
 
-    this.stage.backgroundColor = '#2ECCFA';
+    this.stage.backgroundColor = '#00CC00';
+    //2ECCFA
     this.physics.arcade.gravity.y = 1400;
 
     map = this.add.tilemap('map',64,64);

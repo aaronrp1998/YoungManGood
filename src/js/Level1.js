bf20a128e1654253a7d603e1eb2692461ac2b59a
Xexu98 <jesusf05@ucm.es>
2018-06-26 20:20:55 +0200

    tyhdf

--- src/js/Level1.js
+++ src/js/Level1.js
@@ -254,7 +254,9 @@ Game.Level1.prototype = {
     this.game.debug.text("PosX"+player.body.x,1,100);
     this.game.debug.text("PosY"+player.body.y,1,200);
     this.game.debug.text("Vida"+vidaJugador,1,250);
-    this.game.debug.text("VAR "+enemy.inCamera,1,300)
+    this.game.debug.text("VAR "+enemy.inCamera,1,300);
+    this.game.debug.text("detx "+detectionpointX,1,250);
+    this.game.debug.text("dety "+detectionpointY,1,300)
   },
 
   ResetPosition:function() {

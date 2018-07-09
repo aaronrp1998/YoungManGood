e26953bd80c422ac609555cf1642ff2845c52a40
Xexu98 <jesusf05@ucm.es>
2018-07-09 22:18:11 +0200

    rtjrtj

--- src/js/Preloader.js
+++ src/js/Preloader.js
@@ -34,6 +34,8 @@ Game.Preloader.prototype = {
 
     this.load.image('bossdrop', 'images/bossdrop.png');
 
+    this.load.bitmapFont('font', 'images/carrier_command.png', 'images/carrier_command.xml');
+
     this.load.image('enemy1','images/enemy.png');
 
     this.load.image('enemy2','images/enemy1.png');
